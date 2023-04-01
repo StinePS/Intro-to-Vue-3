ee3f860fafb5689c49fef204c07a3849d4589f9c
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2023-04-01 11:43:30 +0200

    Lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      // Coding challenge lesson 2
+      description: "The warmest socks around!",
+    };
+  },
+});
