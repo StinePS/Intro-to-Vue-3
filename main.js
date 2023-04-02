18dac9fd3142a0bc0db3d338d6e81696c3c244dc
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2023-04-02 12:03:17 +0200

    Lesson 5

--- main.js
+++ main.js
@@ -5,15 +5,10 @@ const app = Vue.createApp({
       image: "./assets/images/socks_blue.jpg",
       inStock: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
+      sizes: ["S", "M", "L", "XL"],
       variants: [
         { id: 2234, color: "green" },
         { id: 2235, color: "blue" },
-      ],
-      sizes: [
-        { id: 0001, str: "Small" },
-        { id: 0002, str: "Medium" },
-        { id: 0003, str: "Large" },
-        { id: 0004, str: "X-Large" },
       ],
     };
   },
