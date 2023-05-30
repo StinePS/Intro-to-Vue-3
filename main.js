a95b2a4450171e9c12177b41a7caaa0232abddb6
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2023-05-30 16:06:44 +0200

    Lesson 8 - Computed properties

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
-            ]
-        }
+  data() {
+    return {
+      cart: 0,
+      product: "Socks",
+      brand: "Vue Mastery",
+      selectedVariant: 0,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg", quantity: 50 },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg", quantity: 0 },
+      ],
+      // Coding challenge
+      onSale: true,
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
     },
-    methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateImage(variantImage) {
-            this.image = variantImage
-        }
-    }
-})
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  // COMPUTED PROPERTIES
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    // Coding challenge - Add an onSale boolean to the data. Use a computed property to display "brand + product is on sale" whenever onSale is true
+    sale() {
+      if (this.onSale) {
+        return this.brand + " " + this.product + " is on sale!";
+      }
+      return "";
+    },
+  },
+});
