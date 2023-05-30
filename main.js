15688e33c72b249e7b9bce8e3dbc8c4490a4c0ff
Stine Plejdrup Schmidt <stin039m@stud.kea.dk>
2023-05-30 15:25:06 +0200

    Lesson 3 - Attribute binding

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
-      //   coding challenge 3
+      //   Coding challenge - Add a URL to the data object - Use v-bind to bind the URL to an anchor tag's href attribute
       url: "www.google.com",
     };
   },
